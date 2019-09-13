bef2275865433809c1ce95d638c287fe8767aa4e
MarceloAlves8799 <marceloacampos87@gmail.com>
2019-09-12 21:42:47 -0300

    Scripts Interação

---
+++ Assets/Scripts/Mechanics/Disjuntor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disjuntor : MonoBehaviour, IInteract
+{
+
+    public bool canUse { get; set; }
+
+    public void Interaction()
+    {
+
+    }
+}
