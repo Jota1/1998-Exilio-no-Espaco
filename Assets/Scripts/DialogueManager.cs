90ea8e18487432fa4a2dd8f962c43f4558225e3f
João Pedro Sodré <jpasodre@hotmail.com>
2019-10-17 19:19:06 -0300

    Dialogo em texto funfando

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text dialogue_Text;
+    public string[] setences;
+    private int index;
+    public float typingSpeed;
+    public Animator dialogueText_Animator;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type ()
+    {
+        foreach (char letter in setences[index].ToCharArray())
+        {
+            dialogue_Text.text += letter;
+            yield return new WaitForSeconds(typingSpeed);           
+        }        
+    }
+
+    private void Update()
+    {
+        //TESTE
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            StartCoroutine(AnimationControl_DialogueBox());
+        }            
+    }
+
+    IEnumerator AnimationControl_DialogueBox () //colocar ESTE Ienumerator para mostrar a próxima sentença (definir dialogos de forma linear)
+    {
+        dialogueText_Animator.SetTrigger("Open");   //obs: colocar a primeira sentença como vazia (sem nenhuma palavra)
+        yield return new WaitForSeconds(0.7f);
+        NextSentence();
+        yield return new WaitForSeconds(3f);
+        dialogueText_Animator.SetTrigger("Close");
+        yield return new WaitForSeconds(0.5f);
+        dialogue_Text.text = "";
+    }
+
+    public void NextSentence () 
+    {
+        if (index < setences.Length - 1)
+        {
+            index++;
+            dialogue_Text.text = "";
+            StartCoroutine(Type());
+
+        }
+        else
+        {
+            dialogue_Text.text = "";
+        }
+    }    
+}
+
--- Assets/Scripts/Mechanics/OrderCommands.cs
+++ Assets/Scripts/Mechanics/OrderCommands.cs
@@ -5,21 +5,10 @@ using UnityEngine.UI;
 
 public class OrderCommands : MonoBehaviour
 {
-    public GameObject menuCommand;
-    public Text dialogue_Text;
+    public GameObject menuCommand;    
     bool isOpen;
     public static bool comandMenu_Open;
-    [SerializeField]
-    Animator dialogueBox;
-    [SerializeField]
-    GameObject command_AbrirPorta;
-    [SerializeField]
-    GameObject command_LigarEnergia;
-    [SerializeField]
-    GameObject command_Dica;
-    [SerializeField]
-    GameObject command_DesligueEnergia; 
-
+  
     void Start()
     {
         menuCommand.SetActive(false);
@@ -42,96 +31,12 @@ public class OrderCommands : MonoBehaviour
         {
             CloseMenu();
             comandMenu_Open = false;
-        }
-
-        //if (Input.GetMouseButton(1) && isOpen){
-
-        //    menuCommand.SetActive(true);
-
-        //} else if(Input.GetMouseButton(1) && !isOpen)
-        //{
-        //    CloseMenu();
-        //}
-    }    
+        }      
+    }
 
     void CloseMenu()
     {
         menuCommand.SetActive(false);
         isOpen = false;
     }
-
-    public void Command1()
-    {
-        Debug.Log("Abra a porta");
-        dialogueBox.SetBool("Open", true);
-        dialogue_Text.text = "";
-        dialogue_Text.text = "Abrindo a porta";
-
-        command_AbrirPorta.SetActive(true);
-        command_LigarEnergia.SetActive(false);
-        command_DesligueEnergia.SetActive(false);
-        command_Dica.SetActive(false);
-
-        //StartCoroutine(Dialogue_Text_AbrirPorta());
-
-        Invoke("CloseDialogue", 1.8f);
-        CloseMenu();
-    }
-
-    IEnumerator Dialogue_Text_AbrirPorta () 
-    {
-        dialogueBox.SetBool("Open", true);
-        yield return new WaitForSeconds(0.5f);
-        dialogue_Text.text = "";
-        yield return new WaitForSeconds(0.5f);
-        dialogue_Text.text = "Abrindo a porta";
-    }
-
-    public void Command2()
-    {
-        Debug.Log("Liga a energia");
-        dialogueBox.SetBool("Open", true);
-
-        command_AbrirPorta.SetActive(false);
-        command_LigarEnergia.SetActive(true);
-        command_DesligueEnergia.SetActive(false);
-        command_Dica.SetActive(false);
-
-        Invoke("CloseDialogue", 1.8f);
-        CloseMenu();
-    }
-
-    public void Command3()
-    {
-        Debug.Log("Desliga a energia");
-        dialogueBox.SetBool("Open", true);
-
-        command_AbrirPorta.SetActive(false);
-        command_LigarEnergia.SetActive(false);
-        command_DesligueEnergia.SetActive(true);
-        command_Dica.SetActive(false);
-
-        Invoke("CloseDialogue", 1.8f);
-        CloseMenu();
-    }
-
-    public void Command4()
-    {
-        Debug.Log("Dica");
-        dialogueBox.SetBool("Open", true);
-
-        command_AbrirPorta.SetActive(false);
-        command_LigarEnergia.SetActive(false);
-        command_DesligueEnergia.SetActive(false);
-        command_Dica.SetActive(true);
-
-        Invoke("CloseDialogue", 1.8f);
-        CloseMenu();
-    }
-
-    void CloseDialogue ()
-    {
-        dialogueBox.SetBool("Open", false);
-    }
-
 }
