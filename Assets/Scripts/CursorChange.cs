c12f39d5d615c063b09abde4e67dfcb6643021b3
João Pedro Sodré <jpasodre@hotmail.com>
2019-11-05 15:08:24 -0300

    Animação Float + Cursor + Ajustes camera

---
+++ Assets/Scripts/CursorChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorChange : MonoBehaviour 
+{
+    public Texture2D cursorDefault;
+    public Texture2D cursorInteract;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    
+    
+    void Update()
+    {
+        UpdateCursorTexture();
+    }
+
+    void UpdateCursorTexture ()
+    {
+        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit2;
+
+        if (Physics.Raycast (ray2, out hit2, 10000))
+        {
+            if (hit2.transform.tag == "Interagivel")
+            {
+                Cursor.SetCursor (cursorInteract, hotSpot, cursorMode);
+            }
+            else { Cursor.SetCursor(cursorDefault, hotSpot, cursorMode); }
+        }
+    }
+}
