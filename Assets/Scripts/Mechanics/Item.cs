ed3e3b5d7aa071ba3354ca1f362faa6508f0fc7e
MarceloAlves8799 <marceloacampos87@gmail.com>
2019-11-06 07:58:31 -0300

    Feedback

--- Assets/Scripts/Mechanics/Item.cs
+++ Assets/Scripts/Mechanics/Item.cs
@@ -12,11 +12,18 @@ public class Item : MonoBehaviour, IInteract
 
     public bool disappear;
 
+    public bool colectItem;
+
     public PuzzleController puzzleController { get; set; }
+
+    public AudioClip positiveFeedback;
+    public AudioClip negativeFeedback;
 
     void Awake()
     {
         puzzleController = FindObjectOfType<PuzzleController>();
+
+        colectItem = false;
 
         // Alteração
     }
@@ -24,22 +31,29 @@ public class Item : MonoBehaviour, IInteract
     public void Interaction()
     {
 
-        if ((FindObjectOfType<Inventory>().CheckCondition(condition) || condition == "") && puzzleNumber == FindObjectOfType<PuzzleController>().puzzleOrder)
+        if ((FindObjectOfType<Inventory>().CheckCondition(condition) || condition == "") && puzzleNumber == FindObjectOfType<PuzzleController>().puzzleOrder && !colectItem)
         {
+            colectItem = true;
+
             FindObjectOfType<Inventory>().itemList.Add(gameObject.GetComponent<Item>());
+
+            Feedback(positiveFeedback);
 
             if (disappear)
             {
                 gameObject.SetActive(false);
             }
-
-            Feedback();
+            
+        } else
+        {
+            Feedback(negativeFeedback);
         }
     }
 
-    void Feedback()
+    void Feedback(AudioClip feedback)
     {
         // Toca som
+        FindObjectOfType<SoundController>().PlaySFX(feedback);
     }
 
 }
