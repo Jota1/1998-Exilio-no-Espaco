d433d91990be4deb3ff1495885a47334bfd2e44b
MarceloAlves8799 <marceloacampos87@gmail.com>
2019-11-06 09:41:21 -0300

    Cheat e Feedback

---
+++ Assets/Scripts/Controllers/Cheat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat : MonoBehaviour
+{
+    public GameObject player;
+
+    public Transform sala1;
+    public Transform sala2;
+    public Transform sala3;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            player.transform.position = sala1.position;
+            FindObjectOfType<PuzzleController>().puzzleOrder = 1;
+            FindObjectOfType<Inventory>().ClearList();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            player.transform.position = sala2.position;
+            FindObjectOfType<PuzzleController>().puzzleOrder = 2;
+            FindObjectOfType<Inventory>().ClearList();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            player.transform.position = sala3.position;
+            FindObjectOfType<PuzzleController>().puzzleOrder = 3;
+            FindObjectOfType<Inventory>().ClearList();
+        }
+    }
+}
