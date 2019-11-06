386877faff63c2167b7b29fb6a2fd496dc7adb0b
MarceloAlves8799 <marceloacampos87@gmail.com>
2019-11-06 01:26:05 -0300

    Puzzle 1

--- Assets/Scripts/Mechanics/SetLight.cs
+++ Assets/Scripts/Mechanics/SetLight.cs
@@ -15,9 +15,18 @@ public class SetLight : MonoBehaviour
     public Item fusivel1;
 
 
+    private void Start()
+    {
+        lightSet1 = true;
+        lightSet2 = true;
+        lightSet3 = true;
+        lightSet4 = true;
+        lightSet5 = true;
+    }
+
     public void FinishSetLight()
     {
-        if(lightSet1 && lightSet2 && lightSet3 && lightSet4 && lightSet5 && !finishMinigame)
+        if(!lightSet1 && !lightSet2 && !lightSet3 && !lightSet4 && !lightSet5 && !finishMinigame)
         {
             finishMinigame = true;
             FindObjectOfType<Inventory>().itemList.Add(fusivel1);
@@ -27,25 +36,41 @@ public class SetLight : MonoBehaviour
     public void SetLight1()
     {
         // 4
+        lightSet1 = false;
+        lightSet3 = false;
+        lightSet4 = true;
     }
 
     public void SetLight2()
     {
         // 5
+        lightSet2 = true;
+        lightSet4 = false;
+        lightSet5 = true;
     }
 
     public void SetLight3()
     {
         // 1
+        lightSet1 = false;
+        lightSet3 = false;
+        lightSet4 = true;
     }
 
     public void SetLight4()
     {
         // 2
+        lightSet2 = false;
+        lightSet3 = true;
+        lightSet5 = false;
     }
 
     public void SetLight5()
     {
         // 3
+        lightSet1 = true;
+        lightSet4 = false;
+        lightSet5 = true;
+       
     }
 }
