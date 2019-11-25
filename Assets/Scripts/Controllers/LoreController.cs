6c9279514c3063cdeb0f0a0a39e729e239d00649
MarceloAlves8799 <marceloacampos87@gmail.com>
2019-11-25 12:38:24 -0300

    Implementando Script de controle da Lore

--- Assets/Scripts/Controllers/LoreController.cs
+++ Assets/Scripts/Controllers/LoreController.cs
@@ -6,9 +6,13 @@ public class LoreController : MonoBehaviour
 {
     private AudioSource dialogueAudio;
 
-    public AudioClip[] dialoguesVoice;
+    public AudioClip[] dialogueVoice;
 
     public float[] dialogueTime;
+
+    public int dialoguePlaying;
+
+    public int limitTalk;
 
     private void Awake()
     {
@@ -17,14 +21,22 @@ public class LoreController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && dialoguePlaying == 0)
         {
             StartCoroutine(DialogueEvent());
         }
     }
 
+
     IEnumerator DialogueEvent()
     {
+        while(dialoguePlaying < limitTalk)
+        {
+            dialogueAudio.PlayOneShot(dialogueVoice[dialoguePlaying]);
+            yield return new WaitForSeconds(dialogueTime[dialoguePlaying]);
+            dialoguePlaying++;
+        }
+
         yield return null;
     }
 }
