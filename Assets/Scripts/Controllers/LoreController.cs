460b5615641d14a4ebbc75f971c4887563026708
MarceloAlves8799 <marceloacampos87@gmail.com>
2019-11-25 11:59:27 -0300

    Preparação para os dialogos entre Hal e Caroline

---
+++ Assets/Scripts/Controllers/LoreController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoreController : MonoBehaviour
+{
+    private AudioSource dialogueAudio;
+
+    public AudioClip[] dialoguesVoice;
+
+    public float[] dialogueTime;
+
+    private void Awake()
+    {
+        dialogueAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(DialogueEvent());
+        }
+    }
+
+    IEnumerator DialogueEvent()
+    {
+        yield return null;
+    }
+}
